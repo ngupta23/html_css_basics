27dc952d29e9127cdf0093feb44cf1d4e4a6469f
Nikhil Gupta <mywork.ng@gmail.com>
2024-10-14 14:28:49 -0500

    feat: added basic tailwind and daisyUI css templates

--- html_css_basics/app.py
+++ html_css_basics/app.py
@@ -5,6 +5,22 @@ logger = logging.getLogger("html_css_basics")
 logger.setLevel(logging.INFO)
 
 
+hdrs = [
+    fh.Meta(charset="UTF-8"),
+    fh.Meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+    fh.Meta(name="description", content="Nikhil's Web Page"),
+    fh.Title("Nikhil's Web Page"),
+    fh.Link(
+        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+        rel="stylesheet",
+    ),
+    fh.Link(
+        href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.css",
+        rel="stylesheet",
+    ),
+]
+
+
 def create_app():
     app = fh.FastHTML(
         default_hdrs=False,
@@ -15,7 +31,7 @@ def create_app():
     def root(session):
         logger.warning("Entered Root.")
         return fh.Html(
-            fh.Head(fh.Title("Home")),
+            fh.Head(*hdrs),
             fh.Body(fh.H1("Hello, World!"), fh.P("This is a simple HTML page.")),
         )
 
